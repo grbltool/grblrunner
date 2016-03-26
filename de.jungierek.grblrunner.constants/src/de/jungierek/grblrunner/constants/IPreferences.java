9c7b5a1b6879755407096a271561261c5548f1e6
Andreas <andreas@jungierek.de>
2016-03-26 21:43:12 +0100

    some corrections

--- de.jungierek.grblrunner.constants/src/de/jungierek/grblrunner/constants/IPreferences.java
+++ de.jungierek.grblrunner.constants/src/de/jungierek/grblrunner/constants/IPreferences.java
@@ -1,12 +1,9 @@
 package de.jungierek.grblrunner.constants;
 
-import org.eclipse.swt.SWT;
 
 
 // TODO_PREF refactor to preferences
 public interface IPreferences {
 
-    public static final int GCODE_LARGE_FONT_SIZE = 20;
-    public static final int GCODE_LARGE_FONT_STYLE = SWT.BOLD;
-
+    // this interface is obsolet, used only for quick dev
 }
