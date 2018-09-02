3b8ab4a6c56f13a172b5dca4f7abbaa027760540
Andreas J <andreas@jungierek.de>
2018-09-02 21:06:34 +0200

    changed to new jog commands

--- de.jungierek.grblrunner/src/de/jungierek/grblrunner/handler/GrblMoveCommandHandler.java
+++ de.jungierek.grblrunner/src/de/jungierek/grblrunner/handler/GrblMoveCommandHandler.java
@@ -1,20 +1,36 @@
 package de.jungierek.grblrunner.handler;
 
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.eclipse.core.commands.ParameterizedCommand;
 import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.core.di.extensions.Preference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import de.jungierek.grblrunner.constants.ICommandId;
+import de.jungierek.grblrunner.constants.IConstant;
+import de.jungierek.grblrunner.constants.IPreferenceKey;
 import de.jungierek.grblrunner.service.gcode.IGcodeService;
 import de.jungierek.grblrunner.service.serial.ISerialService;
 
 public class GrblMoveCommandHandler {
 
     private static final Logger LOG = LoggerFactory.getLogger ( GrblMoveCommandHandler.class );
+
+    // preference
+    private double seekFeedrate;
+
+    @Inject
+    public void setSeekFeedrate ( @Preference(nodePath = IConstant.PREFERENCE_NODE, value = IPreferenceKey.MAX_SEEK_FEEDRATE) int feedrate ) {
+
+        LOG.debug ( "setSeekFeedrate: feedrate=" + feedrate );
+
+        seekFeedrate = feedrate;
+
+    }
 
     @Execute
     public void execute ( IGcodeService gcodeService, ParameterizedCommand command, @Named(ICommandId.GRBL_MOVE_AXIS_PARAMETER) String axis,
@@ -23,7 +39,7 @@ public class GrblMoveCommandHandler {
         LOG.debug ( "execute: axis=" + axis + " cmd=" + command + " dir=" + direction + " dist=" + distance );
 
         if ( command != null ) {
-            gcodeService.sendCommandSuppressInTerminal ( "G91 G0 " + axis + direction + distance );
+            gcodeService.sendCommandSuppressInTerminal ( "$J=G21G91F" + seekFeedrate + axis + direction + distance );
         }
 
     }
--- de.jungierek.grblrunner/src/de/jungierek/grblrunner/handler/GrblMoveZeroCommandHandler.java
+++ de.jungierek.grblrunner/src/de/jungierek/grblrunner/handler/GrblMoveZeroCommandHandler.java
@@ -1,21 +1,37 @@
 package de.jungierek.grblrunner.handler;
 
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.eclipse.core.commands.ParameterizedCommand;
 import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.core.di.annotations.Optional;
+import org.eclipse.e4.core.di.extensions.Preference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import de.jungierek.grblrunner.constants.ICommandId;
+import de.jungierek.grblrunner.constants.IConstant;
+import de.jungierek.grblrunner.constants.IPreferenceKey;
 import de.jungierek.grblrunner.service.gcode.IGcodeService;
 import de.jungierek.grblrunner.service.serial.ISerialService;
 
 public class GrblMoveZeroCommandHandler {
 
     private static final Logger LOG = LoggerFactory.getLogger ( GrblMoveZeroCommandHandler.class );
+
+    // preference
+    private double seekFeedrate;
+
+    @Inject
+    public void setSeekFeedrate ( @Preference(nodePath = IConstant.PREFERENCE_NODE, value = IPreferenceKey.MAX_SEEK_FEEDRATE) int feedrate ) {
+
+        LOG.debug ( "setSeekFeedrate: feedrate=" + feedrate );
+
+        seekFeedrate = feedrate;
+
+    }
 
     @Execute
     public void execute ( IGcodeService gcodeService, ParameterizedCommand command, @Optional @Named(ICommandId.GRBL_MOVE_ZERO_AXIS_PARAMETER) String axis ) {
@@ -24,7 +40,7 @@ public class GrblMoveZeroCommandHandler {
 
         if ( command != null ) {
 
-            String gcode = "G90 G0 ";
+            String gcode = "$J=G21G90F" + seekFeedrate;
 
             for ( int i = 0; i < axis.length (); i++ ) {
                 gcode += axis.charAt ( i ) + "0";
